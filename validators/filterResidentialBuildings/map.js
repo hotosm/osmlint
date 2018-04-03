254d69a72cb36f5799ee53a9bbede9559c62982e
Sajjad Anwar <sajjadkm@gmail.com>
2018-04-03 20:25:51 +0530

    return time bin json

--- validators/filterResidentialBuildings/map.js
+++ validators/filterResidentialBuildings/map.js
@@ -1,15 +1,29 @@
 'use strict';
 var turf = require('@turf/turf');
+var moment = require('moment');
 
 module.exports = function(tileLayers, tile, writeData, done) {
   var layer = tileLayers.osm.osm;
   var osmlint = 'filterresidentialbuildings';
   var result = [];
+  var timeBuckets = {};
+
   for (var i = 0; i < layer.features.length; i++) {
     var val = layer.features[i];
     if (val.properties.hasOwnProperty('building') && val.properties['building'] === 'residential') {
       val.properties._osmlint = osmlint;
       result.push(val);
+
+      // populate timeBucket( for edit recency analysis
+      var timestamp = moment(val.properties['@timestamp'] * 1000);
+      var year = timestamp.year();
+      var month = timestamp.month();
+      var key = String(year) + String(month);
+      if (!timeBuckets.hasOwnProperty(key)) {
+        timeBuckets[key] = 1;
+      } else {
+        timeBuckets[key] = timeBuckets[key] + 1;
+      }
     }
   }
 
@@ -18,5 +32,5 @@ module.exports = function(tileLayers, tile, writeData, done) {
     writeData(JSON.stringify(fc) + '\n');
   }
 
-  done(null, null);
+  done(null, timeBuckets);
 };
